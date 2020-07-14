5d395ec9b61d63af0bc6f7653ed0febfc41d0687
EleazarSauz <eleazar48a@gmail.com>
2020-07-13 21:53:06 -0500

    render map

--- pages/index.js
+++ pages/index.js
@@ -1,170 +1,68 @@
-import Head from 'next/head'
+import {useEffect, useRef} from 'react'
 
+ 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    const myMap = useRef(null);
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-      </main>
-
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+    useEffect(() => {
+      console.log('window.google.maps', google.maps)
+      var uluru = {lat: -25.344, lng: 131.036};
+      var map = new google.maps.Map(myMap.current, {zoom: 11, center: uluru});
+      var request = {
+        query: 'hotel',
+        fields: ['name', 'geometry'],
+      };
+      
+      var service = new google.maps.places.PlacesService(map);
+    
+      service.findPlaceFromQuery(request, function(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            console.log('results', results)
+          for (var i = 0; i < results.length; i++) {
+            new google.maps.Marker({position: results[i].geometry.location, map: map});
           }
+          map.setCenter(results[0].geometry.location);
         }
-      `}</style>
+      });
+    }, [])
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+    
+  return (
+    <>
+      <div className="row">
+        <nav id="sidebarMenu" className="col-md-4 col-lg-3 d-md-block collapse pr-0">
+          <div className="sidebar-sticky pt-4">
+            <ul className="nav flex-column">
+              <li className="nav-item">
+                <a className="nav-link active" href="#">
+                  <span data-feather="home"></span>
+                  Rutas
+                </a>
+              </li>
+              <li className="nav-item">
+                <form className="ml-2" onSubmit={e => {
+                  e.preventDefault()
+                  console.log('e.target', e.target)
+                }}>
+                    <div className="form-group">
+                        <label>¿De dónde sales?</label>
+                        <input type="text" placeholder="Origen" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    </div>
+                    <div className="form-group">
+                        <label>¿A dónde te diriges?</label>
+                        <input type="text" placeholder="Destino" className="form-control" id="exampleInputPassword1"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">Buscar Ruta</button>
+                </form>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+        <main role="main" className="col-md-8 ml-sm-auto col-lg-9">
+          <div className="w-100" style={{ height: '100vh' }} ref={myMap}></div>
+        </main>
+      </div>
+    </>
   )
 }
