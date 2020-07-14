eb5cdab5c3f45ee69aa1044e7e4c9891c67b24e3
EleazarSauz <eleazar48a@gmail.com>
2020-07-13 21:25:44 -0500

    initial config

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import '../styles.css'
+
+
+export default function App({ Component, pageProps }) {
+    return (
+    <>
+        <Head>
+            <title>Maps Rutas</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
+            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"/>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+
+            <script src={`https://maps.googleapis.com/maps/api/js?key=${process.env.API_KEY}&libraries=places`}>
+            </script>
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+        </Head>
+
+        <Component {...pageProps} />
+
+    </>
+    )
+}
